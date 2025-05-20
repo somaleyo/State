7a968a7c1a903f90754c593e5f2964e2b729b75f
Emil <somaleyoo123@gmail.com>
2025-05-20 11:28:49 +0200

    card htm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import { useState } from 'react'
-
+import bg from '/src/assets/images/sidebar.svg'
 import './App.css'
 
 function App() {
  
   return (
     <>
+    <div className="container">
+      <div className="leftside">
+        <img src={bg} className='bg' alt="" />
+        <div className="steps">
+           <span className='numero'>1</span>
+           <div className="text">
+           <p>STEP 1</p>
+           <h3>YOUR INFO</h3>
+           </div>
+        </div>
+        <div className="steps">
+           <span className='numero'>1</span>
+           <div className="text">
+           <p>STEP 1</p>
+           <h3>YOUR INFO</h3>
+           </div>
+        </div>
+        <div className="steps">
+           <span className='numero'>1</span>
+           <div className="text">
+           <p>STEP 1</p>
+           <h3>YOUR INFO</h3>
+           </div>
+        </div>
+        <div className="steps">
+           <span className='numero'>1</span>
+           <div className="text">
+           <p>STEP 1</p>
+           <h3>YOUR INFO</h3>
+           </div>
+        </div>
+       
+      </div>
+      <div className="rightside">
+
+      </div>
+    </div>
    
     </>
   )
