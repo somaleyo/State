4e4da9608bbc84ee7079a24b004f009fd5af7376
Emil <somaleyoo123@gmail.com>
2025-05-20 17:18:48 +0200

    merci css

--- src/Components/Thanks/merci.jsx
+++ src/Components/Thanks/merci.jsx
@@ -1,8 +1,14 @@
 import './merci.css'
+import check from '/src/assets/images/thankyou.svg'
 export default function Merci(props) {
     return(
         <>
-            
+        <div className="merci-container">
+            <img src={check} className='merci' alt="" />
+            <h1>Thank you!</h1>
+            <h6>Thanks for confirming your subscription! We hope you have fun using our platform. if you ever need support, please feel free to contact our support at support@lommgaming.com</h6>
+        </div>
+           
         </>
     )
 }
